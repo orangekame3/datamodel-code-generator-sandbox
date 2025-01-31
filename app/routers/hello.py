3b48e888b66c6bed1f880f3f2a8261c6a70444fd
orangekame3 <miya.org.0309@gmail.com>
2025-01-31 14:26:08 +0900

    add error handling

--- app/routers/hello.py
+++ app/routers/hello.py
@@ -1,12 +1,15 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from ..schemas.hello import SayHelloResponse  # type: ignore
 from ..schemas.error import InternalServerError
 
 router: APIRouter = APIRouter()
 
-@router.get("/hello/{name}", response_model=SayHelloResponse | InternalServerError)
-def say_hello(name: str) -> SayHelloResponse | InternalServerError:
+@router.get("/hello/{name}", response_model=SayHelloResponse)
+def say_hello(name: str) -> SayHelloResponse:
     try:
         return SayHelloResponse(message=f"Hello, {name}!")
     except Exception as e:
-        return InternalServerError(message=str(e))
+        raise HTTPException(
+            status_code=500,
+            detail=InternalServerError(message=str(e)).model_dump()
+        )
